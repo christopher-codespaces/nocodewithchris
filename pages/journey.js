a063fb6ae78f6112ace9b7f135f15800cec593fb
Christopher Makombe <125880140+christopher-codespaces@users.noreply.github.com>
2023-07-20 23:43:09 +0200

    Update journey.js

--- pages/journey.js
+++ pages/journey.js
@@ -1,10 +1,22 @@
 import Image from 'next/image'
 import { Timeline} from '@/components'
+import Head from 'next/head'
 
 
 export default function Journey() {
   return (
     <>
+     <Head>
+        <title>
+          {" "}
+          Welcome To Christopher Makombe
+        </title>
+        <meta
+          name="description"
+          content="A young entrepenuer taking charge of his dreams until he does.This is my journey"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Timeline />
 
    </>
