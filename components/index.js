28400d3abc387bf2e01553532d30a7ded3505270
Christopher Makombe <125880140+christopher-codespaces@users.noreply.github.com>
2024-04-08 15:04:43 +0200

    killhsihdijhdi

--- components/index.js
+++ components/index.js
@@ -7,4 +7,5 @@ export {default as Timeline} from "./Timeline.jsx";
 export {default as Me} from "./Me.jsx";
 export {default as FAQ} from "./FAQ.jsx";
 export {default as WhatsApp} from "./Whatsapp.jsx";
+export {default as Form} from "./Form.jsx";
 
